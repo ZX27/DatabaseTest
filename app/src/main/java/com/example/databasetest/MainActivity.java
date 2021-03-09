6b31093cf139250c73b497fec05c0d161de3c816
ZX27 <43877472+ZX27@users.noreply.github.com>
2021-03-09 21:30:11 +0800

    first commit

---
+++ app/src/main/java/com/example/databasetest/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.databasetest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private MyDatabaseHelper dbHelper;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button createDatabase = (Button) findViewById(R.id.create_database);
+        dbHelper = new MyDatabaseHelper(this, "BookStore.db", null, 1);
+        createDatabase.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dbHelper.getWritableDatabase();
+            }
+        });
+    }
+}
