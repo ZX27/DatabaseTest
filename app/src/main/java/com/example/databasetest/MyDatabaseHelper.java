b9853b668033b958a6df963dee95000452f2d8df
ZX27 <43877472+ZX27@users.noreply.github.com>
2021-03-10 11:17:49 +0800

    对数据的各种操作

--- app/src/main/java/com/example/databasetest/MyDatabaseHelper.java
+++ app/src/main/java/com/example/databasetest/MyDatabaseHelper.java
@@ -6,15 +6,16 @@ import android.database.sqlite.SQLiteOpenHelper;
 import android.widget.Toast;
 
 public class MyDatabaseHelper extends SQLiteOpenHelper {
+
     public static final String CREATE_BOOK = "create table Book ("
             + "id integer primary key autoincrement, "
             + "author text, "
             + "price real, "
-            + "pages integer,"
+            + "pages integer, "
             + "name text)";
 
     public static final String CREATE_CATEGORY = "create table Category ("
-            + "id integer primary key autoincrement,"
+            + "id integer primary key autoincrement, "
             + "category_name text, "
             + "category_code integer)";
 
@@ -27,20 +28,17 @@ public class MyDatabaseHelper extends SQLiteOpenHelper {
     }
 
     @Override
-    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
-        sqLiteDatabase.execSQL("drop table if exists Book");
-        sqLiteDatabase.execSQL("drop table if exists Category");
-        onCreate(sqLiteDatabase);
-    }
-
-    @Override
     public void onCreate(SQLiteDatabase db) {
         db.execSQL(CREATE_BOOK);
         db.execSQL(CREATE_CATEGORY);
         Toast.makeText(mContext, "Create succeeded", Toast.LENGTH_SHORT).show();
-
-
     }
 
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("drop table if exists Book");
+        db.execSQL("drop table if exists Category");
+        onCreate(db);
+    }
 
 }
