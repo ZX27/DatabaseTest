7d45a213c564579b49a3050a6cbbf5f7e6151833
ZX27 <43877472+ZX27@users.noreply.github.com>
2021-03-10 10:44:54 +0800

    插入数据

--- app/src/main/java/com/example/databasetest/MainActivity.java
+++ app/src/main/java/com/example/databasetest/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.databasetest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -15,6 +17,28 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button createDatabase = (Button) findViewById(R.id.create_database);
+        Button addData=(Button) findViewById(R.id.add_data);
+        addData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SQLiteDatabase db = dbHelper.getWritableDatabase();
+                ContentValues values = new ContentValues();
+                //开始组装第一条数据
+                values.put("name", "The Da Vinci Code");
+                values.put("author", "Dan Brown");
+                values.put("pages", 454);
+                values.put("price", 16.96);
+                db.insert("Book", null, values); //插入第一条数据
+                values.clear();
+
+                //开始组装第二条数据
+                values.put("name", "The Lost symbol");
+                values.put("author", "Dan Brown");
+                values.put("pages", 510);
+                values.put("price", 19.95);
+                db.insert("Book", null, values); //插入第二条数据
+            }
+        });
         dbHelper = new MyDatabaseHelper(this, "BookStore.db", null, 2);
         createDatabase.setOnClickListener(new View.OnClickListener() {
             @Override
